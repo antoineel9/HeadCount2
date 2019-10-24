66aee4585b6ffb23a50054a88a7713b8b6b14da1
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-24 15:34:29 -0500

    Add files via upload

---
+++ Data/Audit.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeadCount.Data
+{
+    public class Audit
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        public string Date { get; set; }
+        public string Action { get; set; }
+    }
+}
---
+++ Data/Department.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeadCount.Data
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DeptId { get; set; }
+
+        public string Name { get; set; }
+        public string DeptHead { get; set; }
+        public string DeptLoc { get; set; }
+
+        //foreign key connect Employees table
+
+        public int EmpId { get; set; }
+
+        [ForeignKey("EmpId")]
+        public Employee Employees { get; set; }
+    }
+}
